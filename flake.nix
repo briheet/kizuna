2364d446415baf5f409a31f7aca542d580a1ab84
briheet <briheetyadav@gmail.com>
2026-07-08 10:59:31 +0530

    upd: add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Base flake for Kizuna: An Engineering Memory Graph";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          cowsay
+        ];
+
+      };
+
+    };
+
+}
